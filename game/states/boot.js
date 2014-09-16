c368eef08937b63acb0b75d51282bff75b58a372
SBR <sharpebr@icloud.com>
2014-09-16 07:54:44 -0700

    initial commit

---
+++ game/states/boot.js
@@ -0,0 +1,18 @@
+
+'use strict';
+
+function Boot() {
+}
+
+Boot.prototype = {
+  preload: function() {
+    this.load.image('preloader', 'assets/progressBar.png');
+  },
+  create: function() {
+    this.game.stage.backgroundColor = '#000000';
+    this.game.input.maxPointers = 1;
+    this.game.state.start('preload');
+  }
+};
+
+module.exports = Boot;
