490173e4f5db81b9cf7da4e4a13e65dd8390bb85
SBR <sharpebr@icloud.com>
2014-09-16 09:07:36 -0700

    cleaned up play.js and added keyboard input

--- dist/js/game.js
+++ dist/js/game.js
@@ -100,16 +100,16 @@ module.exports = Menu;
   Play.prototype = {
     create: function() {
       this.game.physics.startSystem(Phaser.Physics.ARCADE);
-      this.sprite = this.game.add.sprite(this.game.width/2, this.game.height/2, 'yeoman');
-      this.sprite.inputEnabled = true;
+      this.cursor = this.game.input.keyboard.createCursorKeys();
+      this.input.keyboard.addKeyCapture([Phaser.Keyboard.UP, Phaser.Keyboard.DOWN, Phaser.Keyboard.RIGHT, Phaser.Keyboard.LEFT]);
       
-      this.game.physics.arcade.enable(this.sprite);
-      this.sprite.body.collideWorldBounds = true;
-      this.sprite.body.bounce.setTo(1,1);
-      this.sprite.body.velocity.x = this.game.rnd.integerInRange(-500,500);
-      this.sprite.body.velocity.y = this.game.rnd.integerInRange(-500,500);
+      this.wasd = {
+        up: this.game.input.keyboard.addKey(Phaser.Keyboard.W),
+        left: this.game.input.keyboard.addKey(Phaser.Keyboard.A),
+        right: this.game.input.keyboard.addKey(Phaser.Keyboard.D),
+        down: this.game.input.keyboard.addKey(Phaser.Keyboard.S),
+      };
 
-      this.sprite.events.onInputDown.add(this.clickListener, this);
     },
     update: function() {
 
@@ -120,6 +120,7 @@ module.exports = Menu;
   };
   
   module.exports = Play;
+
 },{}],6:[function(require,module,exports){
 
 'use strict';
